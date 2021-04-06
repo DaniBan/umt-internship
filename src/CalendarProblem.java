b11faa9f7f6a786c70a15c847735e8f8d56cfa86
Daniel Bancos <57346365+DaniBan@users.noreply.github.com>
2021-04-06 09:17:26 +0300

    refactored lower bound checking and the second part of the algorithm

--- src/CalendarProblem.java
+++ src/CalendarProblem.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
 public class CalendarProblem {
 
     static class Prog{
@@ -13,31 +16,33 @@ public class CalendarProblem {
         }
     }
 
-    static void solve(Prog[] c1, Prog[] c2, float min1, float max1, float min2, float max2){
+    static void solve(Prog[] c1, Prog[] c2, float min1, float max1, float min2, float max2, float threshold){
         List<Prog> result = new ArrayList<Prog>();
 
 
         System.out.println(c1.length + " " + c2.length);
 
-        //TODO add lower and higher bound parameters and also minimum time gap
-
         // check free time with lower bound
-        if((c1[0].start - min1 >= 0.5) && (c2[0].start - min2 >= 0.5)){
-            if(c2[0].start - min1 >= 0.5f){
+        if((c1[0].start - min1 >= threshold) && (c2[0].start - min2 >= threshold)){
+            if(c2[0].start - min1 >= threshold || c1[0].start - min2 >= threshold){
                 if(c1[0]. start < c2[0].start){
-                    result.add(new Prog(min1, c1[0].start));
+                    if(c1[0].start - max(min1, min2) >= threshold){
+                        result.add(new Prog(max(min1, min2), c1[0].start));
+                    }
                 } else {
-                    result.add(new Prog(min1, c2[0].start));
+                    if(c2[0].start - max(min1, min2) >= threshold){
+                        result.add(new Prog(max(min1, min2), c2[0].start));
+                    }
                 }
             }
 
-            if(c1[0].start - min2 >= 0.5f){
-                if(c2[0].start < c1[0].start){
-                    result.add(new Prog(min2, c2[0].start));
-                } else {
-                    result.add(new Prog(min2, c1[0].start));
-                }
-            }
+//            if(c1[0].start - min2 >= 0.5f){
+//                if(c2[0].start < c1[0].start){
+//                    result.add(new Prog(max(min1, min2), c2[0].start));
+//                } else {
+//                    result.add(new Prog(max(min1, min2), c1[0].start));
+//                }
+//            }
         }
 
         int i = 0, j = 0;
@@ -47,13 +52,15 @@ public class CalendarProblem {
             System.out.println("[i, j] = [" + i + ", " + j + "]");
             if(c1[i].end < c2[j].start){
 //                System.out.println("i = " + i);
-                if(c2[j].start - c1[i].end >= 0.5f){
+                if(c2[j].start - c1[i].end >= threshold){
                     if(i + 1 < c1.length){
-                        if(c1[i+1].start - c1[i].end >= 0.5f && c1[i+1].start > c2[j].start){
+                        if(c1[i+1].start - c1[i].end >= threshold && c1[i+1].start > c2[j].start){
                             result.add(new Prog(c1[i].end, c2[j].start));
 //                            System.out.print("[" + c1[i].end + ", " + c2[j].start + "] ");
                         } else {
-                            result.add(new Prog(c1[i].end, c1[i + 1].start));
+                            if(c1[i+1].start - c1[i].end >= threshold){
+                                result.add(new Prog(c1[i].end, c1[i + 1].start));
+                            }
                         }
                     } else {
                         result.add(new Prog(c1[i].end, c2[j].start));
@@ -67,13 +74,15 @@ public class CalendarProblem {
 
             if(c2[j].end < c1[i].start){
 //                System.out.println("j = " + j);
-                if(c1[i].start - c2[j].end >= 0.5f){
+                if(c1[i].start - c2[j].end >= threshold){
                     if(j + 1 < c2.length){
-                        if(c2[j+1].start - c2[j].end >= 0.5f && c2[j+1].start > c1[i].start){
+                        if(c2[j+1].start - c2[j].end >= threshold && c2[j+1].start > c1[i].start){
                             result.add(new Prog(c2[j].end, c1[i].start));
 //                            System.out.print("[" + c2[j].end + ", " + c1[i].start + "] ");
                         } else {
-                            result.add(new Prog(c2[j].end, c2[j + 1].start));
+                            if(c2[j+1].start - c2[j].end >= threshold){
+                                result.add(new Prog(c2[j].end, c2[j + 1].start));
+                            }
                         }
                     } else {
                         result.add(new Prog(c2[j].end, c1[i].start));
@@ -106,30 +115,56 @@ public class CalendarProblem {
         System.out.println(i + " " + j);
 
         int check = 0;
-
-        //TODO ceva susta aicea ce tre rezolvata
+        int ok = 0;
 
         while(i < c1.length){
 
-            if(i + 1 < c1.length){
-                if(check == 0){
-                    if((c1[i+1].start - c1[i].end >= 0.5f) && (c1[i].start - c2[j].end >= 0.5f)){
-                        result.add(new Prog(c2[j].end, c1[i].start));
-//                            System.out.print("[" + c1[i].end + ", " + c2[j].start + "] ");
-                        check = 1;
-                    }
-                } else {
-                    if(c1[i+1].start - c1[i].end >= 0.5f && (max2 - c1[i + 1].start >= 0.5f)){
-                        result.add(new Prog(c1[i].end, c1[i+1]. start));
-                    }
-                }
-
-            } else {
-                if(c1[i].start - c2[j].end >= 0.5f && (c1[i].start - c1[i - 1].end >= 0.5f))
-                result.add(new Prog(c2[j].end, c1[i].start));
-//                        System.out.print("[" + c1[i].end + ", " + c2[j].start + "] ");
-                check = 1;
+            if(i - 1 < 0){
+                i++;
+                continue;
             }
+
+            if(check == 0){
+                if(c1[i].start - c1[i-1].end >= threshold && c1[i].start - c2[j].end >= threshold){
+                    result.add(new Prog(max(c2[j].end, c1[i-1].end), c1[i].start));
+//                        System.out.print("[" + c1[i].end + ", " + c2[j].start + "] ");
+                    check = 1;
+                }
+            } else {
+                if(c1[i].start - c1[i-1].end >= threshold){
+                    result.add(new Prog(c1[i-1].end, c1[i].start));
+                }
+            }
+
+//            if(i + 1 < c1.length){
+//                if(check == 0){
+//                    if((c1[i+1].start - c1[i].end >= threshold) && (c1[i].start - c2[j].end >= threshold)){
+//                        result.add(new Prog(max(c2[j].end, c1[i-1].end), c1[i].start));
+////                        System.out.print("[" + c1[i].end + ", " + c2[j].start + "] ");
+//                        check = 1;
+//                    }
+//                } else {
+//                    if(c1[i+1].start - c1[i].end >= threshold){
+//                        result.add(new Prog(c1[i].end, c1[i+1]. start)); //HERE
+//                        ok = 1;
+//                    }
+//                }
+//
+//            } else {
+//                if(c1[i].start - c2[j].end >= threshold && (c1[i].start - c1[i - 1].end >= threshold)){
+//                    if(c1[i-1].end < c2[j].end){
+//                        if(check == 0){
+//                            result.add(new Prog(c2[j].end, c1[i].start));
+//                        }
+//                    } else {
+//                        if(ok == 0){
+//                            result.add(new Prog(c1[i-1].end, c1[i].start)); //HERE
+//                        }
+//                    }
+//                }
+////                System.out.println("[" + c2[j].end + ", " + c1[i].start + "] ");
+//                check = 1;
+//            }
             i++;
         }
 
@@ -142,26 +177,52 @@ public class CalendarProblem {
         }
 
         check = 0;
+        ok = 0;
         while(j < c2.length){
-            if(j + 1 < c2.length){
-                if(check == 0){
-                    if((c2[j+1].start - c2[j].end >= 0.5f) && (c2[j].start - c1[i].end >= 0.5f)){
-                        result.add(new Prog(c1[i].end, c2[j].start));
-//                            System.out.print("[" + c1[i].end + ", " + c2[j].start + "] ");
-                        check = 1;
-                    }
-                } else {
-                    if(c2[j+1].start - c2[j].end >= 0.5f && (max1 - c2[j + 1].start >= 0.5f)){
-                        result.add(new Prog(c2[j].end, c2[j+1]. start));
-                    }
-                }
 
-            } else {
-                if(c2[j].start - c1[i].end >= 0.5f && (c2[j].start - c2[j-1].end >= 0.5f))
-                    result.add(new Prog(c1[i].end, c2[j].start));
-//                        System.out.print("[" + c1[i].end + ", " + c2[j].start + "] ");
-                check = 1;
+            if(j - 1 < 0){
+                j++;
+                continue;
             }
+
+            if(check == 0){
+                if(c2[j].start - c2[j-1].end >= threshold && c2[j].start - c1[i].end >= threshold){
+                    result.add(new Prog(max(c1[i].end, c2[j-1].end), c2[j].start));
+//                        System.out.print("[" + c1[i].end + ", " + c2[j].start + "] ");
+                    check = 1;
+                }
+            } else {
+                if(c2[j].start - c2[j-1].end >= threshold){
+                    result.add(new Prog(c2[j-1].end, c2[j].start));
+                }
+            }
+
+//            if(j + 1 < c2.length){
+//                if(check == 0){
+//                    if((c2[j+1].start - c2[j].end >= threshold) && (c2[j].start - c1[i].end >= threshold)){
+//                        result.add(new Prog(c1[i].end, c2[j].start));
+////                            System.out.print("[" + c1[i].end + ", " + c2[j].start + "] ");
+//                        check = 1;
+//                    }
+//                } else {
+//                    if(c2[j+1].start - c2[j].end >= threshold && (max1 - c2[j + 1].start >= threshold)){
+//                        result.add(new Prog(c2[j].end, c2[j+1]. start));
+//                        ok = 1;
+//                    }
+//                }
+//
+//            } else {
+//                if(c2[j].start - c1[i].end >= threshold && (c2[j].start - c2[j-1].end >= threshold)) {
+//                    if(c2[j-1].end < c1[i].end){
+//                        result.add(new Prog(c1[i].end, c2[j].start));
+//                    } else {
+//                        if(ok == 0){
+//                            result.add(new Prog(c2[j-1].end, c2[j].start));
+//                        }
+//                    }
+//                }
+//                check = 1;
+//            }
             j++;
 
         }
@@ -175,12 +236,12 @@ public class CalendarProblem {
         }
 
         // check for higher bound free time
-        if((20.0f - c1[i].start >= 0.5) && (max2 - c2[j].end > 0.5f)){
-            if(c1[i].end > c2[j].end && (max2 - c1[i].end >= 0.5f)){
-                result.add(new Prog(c1[i].end, max2));
+        if((max1 - c1[i].end >= 0.5) && (max2 - c2[j].end > threshold)){
+            if(c1[i].end > c2[j].end && (max2 - c1[i].end >= threshold)){
+                result.add(new Prog(c1[i].end, min(max1, max2)));
             }
-            if(c2[j].end > c1[i].end && (max1 - c2[j].end >= 0.5f)){
-                result.add(new Prog(c2[j].end, max1));
+            if(c2[j].end > c1[i].end && (max1 - c2[j].end >= threshold)){
+                result.add(new Prog(c2[j].end, min(max1, max2)));
             }
         }
 
@@ -197,8 +258,12 @@ public class CalendarProblem {
         Prog[] c3 = {new Prog(9,11), new Prog(12,12.5f), new Prog(13,16), new Prog(19,19.5f)};
         Prog[] c4 = {new Prog(12,12.5f), new Prog(14,15), new Prog(16,19)};
 
-//        solve(c1, c2, 9.0f, 20.0f, 10.0f, 18.5f);
-        solve(c3, c4, 8.0f, 22.0f, 9.0f, 20.0f);
+        Prog[] c5 = {new Prog(10,13), new Prog(13.5f,14), new Prog(15,17), new Prog(18,18.5f)};
+        Prog[] c6 = {new Prog(11,12)};
+
+//        solve(c1, c2, 9.0f, 20.0f, 10.0f, 18.5f, 0.5f);
+        solve(c3, c4, 8.0f, 22.0f, 9.0f, 20.0f, 0.5f);
+//        solve(c5, c6, 8.0f, 18.5f, 9.0f, 20.0f, 0.5f);
 
     }
 }
